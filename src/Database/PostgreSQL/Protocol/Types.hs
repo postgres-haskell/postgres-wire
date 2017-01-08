2e1cccec67e6f42eaa8d6138cc9594ab45e2fa9f
VyacheslavHashov <vyacheslavhashov@gmail.com>
2017-01-08 16:51:05 +0300

    More precise base types

--- src/Database/PostgreSQL/Protocol/Types.hs
+++ src/Database/PostgreSQL/Protocol/Types.hs
@@ -7,31 +7,54 @@ import qualified Data.Vector as V
 type PortalName = B.ByteString
 type StatementName = B.ByteString
 type Oid = Word32
-type TransactionStatus = Word8
+-- maybe distinguish sql for extended query and simple query
+type StatementSQL = B.ByteString
+type PasswordText = B.ByteString
+type ServerProccessId = Word32
+type ServerSecretKey = Word32
+-- String that identifies which SQL command was completed.
+-- should be more complex in future
+type CommandTag = B.ByteString
+
+data TransactionStatus
+    = TransactionIdle
+    | TransactionInProgress
+    | TransactionFailed
 
 data Format = Text | Binary
     deriving (Show)
 
+-- All the commands have the same names as presented in the official
+-- postgres documentation except explicit exclusions
+-- TODO improve
 data AuthResponse
     = AuthenticationOk
-    | AuthenticationKerberosV5
     | AuthenticationCleartextPassword
     | AuthenticationMD5Password Word32
-    | AuthenticationSCMCredential
     | AuthenticationGSS
     | AuthenticationSSPI
     | AuthenticationGSSContinue B.ByteString
 
 data ClientMessage
-    = Bind PortalName StatementName (V.Vector Format) (V.Vector B.ByteString)
-                                    (V.Vector Format)
-    | Close B.ByteString
-    | Describe B.ByteString
-    | Execute PortalName Word32
+    = Bind PortalName StatementName
+        (V.Vector Format)       -- parameter format codes
+        (V.Vector B.ByteString) -- the values of parameters
+        (V.Vector Format)       -- format codes of result,
+                                -- maybe chaneg to one number
+                                -- to apply code to all result columns
+    -- Postgres use one command `close` for closing both statements and
+    -- portals, but we distinguish them
+    | CloseStatement StatementName
+    | ClosePortal PortalName
+    -- Postgres use one commande `describe` for describing both statements
+    -- and portals, but we distinguish them
+    | DescribeStatement StatementName
+    | DescribePortal PortalName
+    | Execute PortalName
     | Flush
-    | Parse StatementName B.ByteString (V.Vector Oid)
-    | PasswordMessage B.ByteString
-    | Query B.ByteString
+    | Parse StatementName StatementSQL (V.Vector Oid)
+    | PasswordMessage PasswordText
+    | Query StatementSQL
     | Sync
     | Terminate
 
@@ -45,17 +68,23 @@ data StartMessage
 
 
 data ServerMessage
-    = BackendKeyData Word32
+    = BackendKeyData ServerProccessId ServerSecretKey
     | BindComplete
     | CloseComplete
-    | CommandComplete B.ByteString
-    | DataRow B.ByteString
+    | CommandComplete CommandTag
+    | DataRow (V.Vector B.ByteString) -- the values of a result
     | EmptyQueryResponse
+    -- TODO change to list of error fields
     | ErrorResponse (Maybe B.ByteString)
     | NoData
+    -- TODO change to list of fields
     | NoticeResponse (Maybe B.ByteString)
-    | NotificationResponse Word32 B.ByteString B.ByteString
+    | NotificationResponse
+        ServerProccessId
+        B.ByteString -- the name of the channel
+        B.ByteString -- payload - does not have structure
     | ParameterDescription (V.Vector Oid)
+    -- parameter name and its value
     | ParameterStatus B.ByteString B.ByteString
     | ParseComplete
     | PortalSuspended
@@ -65,16 +94,27 @@ data ServerMessage
 
 data FieldDescription = FieldDescription
     { fieldName :: B.ByteString
+    -- the object ID of the table
     , fieldTableOid :: Oid
+    --  the attribute number of the column;
     , fieldColumnNumber :: Word16
     , fieldTypeOid :: Oid
+    -- The data type size (see pg_type.typlen). Note that negative
+    -- values denote variable-width types.
     , fieldSize :: Word16
+    -- The type modifier (see pg_attribute.atttypmod).
     , fieldMode :: Word32
+    -- In a RowDescription returned from the statement variant of Describe,
+    -- the format code is not yet known and will always be zero.
     , fieldFormat :: Format
     } deriving (Show)
 
 -- TODO
--- | CancelRequest
--- COPY subprotocol commands
--- function call
+-- * CancelRequest
+-- * COPY subprotocol commands
+-- * function call, is deprecated by postgres
+-- * AuthenticationKerberosV5 IS deprecated by postgres
+-- * AuthenticationSCMCredential IS deprecated since postgres 9.1
+-- * NOTICE execute command can have number of rows to receive, but we
+--   dont support this feature
 
