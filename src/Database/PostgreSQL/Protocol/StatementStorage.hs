47e3488fd3965f7cb95b38324e826b3636d72475
VyacheslavHashov <vyacheslavhashov@gmail.com>
2017-01-16 20:58:12 +0300

    Prepared statement storage

---
+++ src/Database/PostgreSQL/Protocol/StatementStorage.hs
@@ -0,0 +1,28 @@
+module Database.PostgreSQL.Protocol.StatementStorage where
+
+import qualified Data.HashTable.IO as H
+import qualified Data.ByteString as B
+import Data.ByteString.Char8 (pack)
+import Data.Word (Word)
+import Data.IORef
+
+import Database.PostgreSQL.Protocol.Types
+
+-- | Prepared statement storage
+data StatementStorage = StatementStorage
+    (H.CuckooHashTable StatementSQL StatementName) (IORef Word)
+
+newStatementStorage :: IO StatementStorage
+newStatementStorage = StatementStorage <$> H.new <*> newIORef 0
+
+lookupStatement :: StatementStorage -> StatementSQL -> IO (Maybe StatementName)
+lookupStatement (StatementStorage table _) = H.lookup table
+
+storageStatement :: StatementStorage -> StatementSQL -> IO StatementName
+storageStatement (StatementStorage table counter) stmt = do
+    n <- readIORef counter
+    writeIORef counter $ n + 1
+    let name = pack $ show n
+    H.insert table name stmt
+    pure name
+
