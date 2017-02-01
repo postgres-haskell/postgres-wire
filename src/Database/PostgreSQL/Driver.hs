fe7e7f4c3994a1ef1e788316d8be8616dc758445
VyacheslavHashov <vyacheslavhashov@gmail.com>
2017-02-02 02:41:00 +0300

    Public API

--- src/Database/PostgreSQL/Driver.hs
+++ src/Database/PostgreSQL/Driver.hs
@@ -1,5 +1,40 @@
-module Database.PostgreSQL.Driver where
+module Database.PostgreSQL.Driver
+    (
+    -- * Settings
+      ConnectionSettings(..)
+    , TlsMode
+    , defaultConnectionSettings
+    -- * Connection
+    , Connection
+    , connect
+    , close
+    -- * Information about connection
+    , getServerVersion
+    , getServerEncoding
+    , getIntegerDatetimes
+    -- * Queries
+    , Query(..)
+    , Oid(..)
+    , Format(..)
+    , sendBatch
+    , sendBatchAndSync
+    , sendBatchAndFlush
+    , sendSync
+    , sendFlush
+    , readNextData
+    , readReadyForQuery
+    , sendSimpleQuery
+    , describeStatement
+    -- * Errors
+    , Error(..)
+    , AuthError(..)
+    , ErrorDesc(..)
+    ) where
+
+import Database.PostgreSQL.Protocol.Types
 
 import Database.PostgreSQL.Driver.Connection
 import Database.PostgreSQL.Driver.Settings
+import Database.PostgreSQL.Driver.Query
+import Database.PostgreSQL.Driver.Error
 
