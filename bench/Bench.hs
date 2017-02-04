48d7e5247d5b13ca38f18927cb75664b0d8f1482
VyacheslavHashov <vyacheslavhashov@gmail.com>
2017-02-04 07:30:54 +0300

    Simple benchmark for protocol encoding

---
+++ bench/Bench.hs
@@ -0,0 +1,52 @@
+module Main where
+
+import Data.ByteString.Lazy (toStrict)
+import Data.ByteString.Builder (toLazyByteString)
+import Data.ByteString (ByteString)
+import Data.Vector as V(fromList, empty)
+
+import Database.PostgreSQL.Protocol.Types
+import Database.PostgreSQL.Protocol.Encoders
+import Criterion.Main
+
+main = defaultMain
+    [ bgroup "Protocol encoding"
+        [ benchMessage "Flush" Flush
+        , benchMessage "Execute" $ Execute (PortalName "") noLimitToReceive
+        , benchParse0
+        , benchParse3
+        , benchParse5
+        , benchParse10
+        , benchBind0
+        , benchBind3
+        , benchBind5
+        , benchBind10
+        ]
+    ]
+
+benchMessage :: String -> ClientMessage -> Benchmark
+benchMessage name = bench name . nf encodeMessage
+  where
+    encodeMessage = toStrict . toLazyByteString . encodeClientMessage
+
+parseMessage :: Int -> ClientMessage
+parseMessage n = Parse (StatementName "53") (StatementSQL
+    "SELECT type, name, a, b, c FROM table_name WHERE name LIKE $1 AND a > $2")
+    (V.fromList $ replicate n (Oid 23))
+
+bindMessage :: Int -> ClientMessage
+bindMessage n = Bind (PortalName "") (StatementName "31") Binary
+    (V.fromList $ replicate n "aaaaaaaaaaaaaaaaaaa") Binary
+
+benchParse0, benchParse3, benchParse5, benchParse10 :: Benchmark
+benchParse0 = benchMessage "Parse 0" $ parseMessage 0
+benchParse3 = benchMessage "Parse 3" $ parseMessage 3
+benchParse5 = benchMessage "Parse 5" $ parseMessage 5
+benchParse10 = benchMessage "Parse 10" $ parseMessage 10
+
+benchBind0, benchBind3, benchBind5, benchBind10 :: Benchmark
+benchBind0 = benchMessage "Bind 0" $ bindMessage 0
+benchBind3 = benchMessage "Bind 3" $ bindMessage 3
+benchBind5 = benchMessage "Bind 5" $ bindMessage 5
+benchBind10 = benchMessage "Bind 10" $ bindMessage 10
+
